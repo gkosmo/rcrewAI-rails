51fd5cae1ab2a15e754f439590ee4ab18d01c148
george kosmopoulos <gkosmo1@hotmail.com>
2026-07-06 10:28:36 +0200

    Include execution batch_id in install generator

--- lib/generators/rcrewai/rails/install/templates/create_rcrewai_tables.rb
+++ lib/generators/rcrewai/rails/install/templates/create_rcrewai_tables.rb
@@ -102,12 +102,14 @@ class CreateRcrewaiTables < ActiveRecord::Migration[7.0]
       t.datetime :started_at
       t.datetime :completed_at
       t.integer :duration_seconds
+      t.string :batch_id
 
       t.timestamps
     end
 
     add_index :rcrewai_executions, :status
     add_index :rcrewai_executions, :created_at
+    add_index :rcrewai_executions, :batch_id
 
     create_table :rcrewai_execution_logs do |t|
       t.references :execution, null: false, foreign_key: { to_table: :rcrewai_executions }
